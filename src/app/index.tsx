0948d18478319fef960acbbafc37c2e091ce84a2
Felipe Amaral <3482714@cefetes.br>
2025-09-29 17:37:32 -0300

    passando parametro para as rotas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import { router } from "expo-router";
 
-export default function index() {
+export default function Index() {
     return (
         <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
             <Text>Hello World  </Text>
+
+            <Button title="Ir para Target" onPress={() => { router.push('/target') }} />
+            <Button title="Transação" onPress={() => router.navigate("/transaction/1")}  />
         </View>
     )
 
