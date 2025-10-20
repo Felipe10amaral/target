217f58f4c9a017442fc8fab3e1f1db9d37d8e97a
Felipe Amaral <3482714@cefetes.br>
2025-10-20 10:31:05 -0300

    componente Target

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { Button, Text, View } from "react-native";
 import { router } from "expo-router";
 import { HomeHeader } from "@/components/HomeHeader";
+import { Target } from "@/components/Target";
 
 const summary = {
     total: "R$ 1000,00",
@@ -8,11 +9,23 @@ const summary = {
     output: { label: 'Saídas', value: '- R$ 1.400,00' }
 }
 
+const targets = [
+    {
+        name: 'Meta de Viagem',
+        percentage: '20%',
+        current: 'R$ 3.400,00',
+        target: 'R$ 17.000,00'
+    }
+]
+
 export default function Index() {
     return (
         <View style={{flex:1}}>
             <HomeHeader data={ summary } />
       
+            <Target 
+                data={targets[0]}
+            />
         </View>
     )
 
--- src/components/Target/styles.ts
+++ src/components/Target/styles.ts
@@ -2,8 +2,27 @@ import { StyleSheet } from 'react-native'
 import {colors, fontFamily} from '@/theme'
 
 export const styles = StyleSheet.create({
-    container: {},
-    content: {},
-    name: {},
-    status: {}
+    container: {
+        height: 72,
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 12,
+        paddingBottom: 16
+    },
+    content: {
+        flex: 1,
+        gap: 7,
+
+    },
+    name: {
+        fontFamily: fontFamily.medium,
+        fontSize: 16,
+        color: colors.black
+    },
+    status: {
+        fontSize: 14,
+        color: colors.gray[500],
+        fontFamily: fontFamily.regular
+    }
 })
