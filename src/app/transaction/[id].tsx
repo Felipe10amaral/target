2d82201990d9ad54aa40f55397e62c953dd5fcbb
Felipe Amaral <felipeamaraldev15@gmail.com>
2025-11-27 15:47:31 -0300

    componente transaction type

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -4,10 +4,14 @@ import {useLocalSearchParams, router} from "expo-router";
 import { PageHeader } from "@/components/PageHeader";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
+import { TransactionType } from "@/components/TransactionType";
+import { useState } from "react";
+import { TransactionTypes } from "@/utils/transactionsTypes";
 
 
 export default function Transaction() {
     const params = useLocalSearchParams<{ id: string }>();
+    const [type, setType] = useState(TransactionTypes.Input)
     return (
         <View style={{ flex: 1, padding: 24}}>
             <PageHeader 
@@ -15,6 +19,11 @@ export default function Transaction() {
                 subtitle="A cada valor guardado você fica mais próximo dos seus objetivos"
             />
             <View  style={{ marginTop: 32, gap: 24 }}>
+                <TransactionType 
+                    selected={type}
+                    onChange={setType}
+                />
+                
                 <CurrencyInput value={0} label="Valor" />
                 <Input label="Motivo (opcional)" placeholder="Ex.: Investir em CDB" />
                 
