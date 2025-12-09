adab097b70786a3d6b2b572db5a95835ab51f1f5
Felipe Amaral <felipeamaraldev15@gmail.com>
2025-12-09 17:17:55 -0300

    função salvar

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,11 +1,47 @@
-import {View, Text} from 'react-native'
-import { router } from 'expo-router'
+import {View, Text, Alert} from 'react-native'
+import { router, useLocalSearchParams } from 'expo-router'
 import { PageHeader } from '@/components/PageHeader'
 import { Input } from '@/components/Input'
 import { Button } from '@/components/Button'
 import { CurrencyInput } from '@/components/CurrencyInput'
+import { useState } from 'react'
 
 export default function Target() {
+    const [isProcessing, setIsProcessing] = useState(false)
+    const [name, setName] = useState("")
+    const [amount, setAmount] = useState(0)
+
+    const params = useLocalSearchParams<{ id?: string }>()
+
+    function handleSave() {
+        if(!name.trim() || amount <= 0) {
+            return Alert.alert("Atenção", "Preencha todos os campos corretamente.")
+        }
+
+        setIsProcessing(true)
+
+        if(params.id) {
+
+        } 
+        else {
+            create()
+        }
+    }
+
+    async function create() {
+        try {
+            Alert.alert("Sucesso", "Meta criada com sucesso.", [
+                {
+                    text: "OK",
+                    onPress: () => router.back()
+                }
+            ])
+        } catch (error) {
+            Alert.alert("Erro", "Não foi possível criar a meta.")
+            console.log(error)
+            setIsProcessing(false)
+        }
+    }
     return (
         <View style={{ flex: 1, padding: 24 }}>
            <Text>Target</Text>
@@ -18,9 +54,9 @@ export default function Target() {
                 }}           
             />
             <View style={{ marginTop: 32, gap: 24 }}>
-                <Input label='Nome da meta' placeholder='Carro zero'/>
-                <CurrencyInput label="Valor alvo" value={7520} />
-                <Button title="Salvar" onPress={() => router.back()} />
+                <Input value={name} label='Nome da meta' placeholder='Carro zero' onChangeText={setName}/>
+                <CurrencyInput label="Valor alvo" value={amount} onChangeValue={setAmount}/>
+                <Button title="Salvar" onPress={() => handleSave()} isLoading={isProcessing} />
             </View>
             
         </View>
