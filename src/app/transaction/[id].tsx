3b6a7273b108df18c39a3dcf98a30bfa9dad83b5
Felipe Amaral <felipeamaraldev15@gmail.com>
2026-01-16 12:24:39 -0300

    cadastrando transações

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,17 +1,49 @@
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 import { Button } from '../../components/Button'
 import {useLocalSearchParams, router} from "expo-router";
 import { PageHeader } from "@/components/PageHeader";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { TransactionType } from "@/components/TransactionType";
-import { useState } from "react";
+import { use, useState } from "react";
 import { TransactionTypes } from "@/utils/transactionsTypes";
+import { useTransactionsDatabase } from "@/database/useTransactionsDatabase";
 
 
 export default function Transaction() {
     const params = useLocalSearchParams<{ id: string }>();
     const [type, setType] = useState(TransactionTypes.Input)
+    const [isCreating, setIsCreating] = useState(false)
+    const [amount, setAmount] = useState(0)
+    const [observation, setObservation] = useState("")
+
+    const transactions = useTransactionsDatabase()
+
+    async function handleCreate() {
+        try {
+            if(amount <= 0 ){
+                return Alert.alert("Erro", "O valor da transação deve ser maior que zero.")
+            }
+            setIsCreating(true)
+
+            await transactions.create({
+                target_id: Number(params.id),
+                amount: type === TransactionTypes.Output ? amount * -1 : amount,
+                observation
+            })
+            Alert.alert("Sucesso", "Transação criada com sucesso.", [
+                {
+                    text: "Ok",
+                    onPress: () => router.back()
+                }
+            ])
+            
+        } catch (error) {
+            Alert.alert("Erro", "Não foi possível criar a transação.")
+            console.log(error)
+            setIsCreating(false)
+        }
+    }
     return (
         <View style={{ flex: 1, padding: 24}}>
             <PageHeader 
@@ -24,10 +56,10 @@ export default function Transaction() {
                     onChange={setType}
                 />
                 
-                <CurrencyInput value={0} label="Valor" />
-                <Input label="Motivo (opcional)" placeholder="Ex.: Investir em CDB" />
+                <CurrencyInput value={amount} label="Valor" onChangeValue={setAmount}  />
+                <Input label="Motivo (opcional)" placeholder="Ex.: Investir em CDB" onChangeText={setObservation} />
                 
-                <Button title="Salvar" onPress={() => router.back()} />
+                <Button title="Salvar" onPress={handleCreate} isLoading={isCreating} />
             </View>
         </View>
     )
--- src/database/useTransactionsDatabase.ts
+++ src/database/useTransactionsDatabase.ts
@@ -1,12 +1,49 @@
 import {useSQLiteContext} from 'expo-sqlite'
 
+export type TransactionCreate = {
+    target_id : number
+    amount : number
+    observation?: string
+}
+
+export type TransactionsResponse = {
+    id: number
+    target_id : number
+    amount : number
+    observation?: string
+    created_at: Date
+    updated_at: Date
+}
 export function  useTransactionsDatabase() {
     const database = useSQLiteContext()
 
-    async function create(data){}
+    async function create(data){
+        const statement = await database.prepareAsync(
+            `
+            insert into transactions (target_id, amount, observation)
+            values ($target_id, $amount, $observation)
+            `
+        )
+
+        statement.executeAsync({
+            $target_id: data.target_id,
+            $amount: data.amount,
+            $observation: data.observation
+        })
+    }
+
+    async function listByTargetId(id: number) {
+        return database.getAllAsync<TransactionsResponse>(`
+            select id, target_id, amount, observation, created_at, updated_at 
+            from transactions
+            where target_id = ${id}
+            ORDER BY created_at DESC
+        `)
+    }
 
     return {
-        create
+        create,
+        listByTargetId
     }
 }
 
