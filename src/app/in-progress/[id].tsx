1ac98f8e9524d880a9470ae7ff435bdbcde37616
Felipe Amaral <felipeamaraldev15@gmail.com>
2026-01-23 11:28:13 -0300

    formatando data e hora

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -11,7 +11,7 @@ import { TransactionTypes } from "@/utils/transactionsTypes";
 import { useLocalSearchParams, router, useFocusEffect } from "expo-router";
 import { useCallback, useState } from "react";
 import { Alert, View } from "react-native";
-
+import dayjs from "dayjs";
 
 
 export default function InProgress() {
@@ -68,7 +68,7 @@ export default function InProgress() {
                 response.map((item) => ( {
                     id: String(item.id),
                     value: numberToCurrency(item.amount),
-                    date: String(item.created_at),
+                    date: dayjs(item.created_at).format("DD/MM/YYYY [às] HH:mm"),
                     description: item.observation,
                     type: item.amount < 0 ? TransactionTypes.Output : TransactionTypes.Input
                 }))
